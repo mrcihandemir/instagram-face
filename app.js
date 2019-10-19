4001c3c7f31421125e3fd5ff171804eb48b4423c
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-20 00:40:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>profile name: <input type="text" name="ip" id="ip"><br><button onclick="Getir(document.getElementById("ip").value)">Getir</button></h1>');
+  //res.setHeader('Content-Type', 'text/html');
+  //res.end('<h1>profile name: <input type="text" name="ip" id="ip"><br><button onclick="Getir(document.getElementById("ip").value)">Getir</button></h1>');
+  res.writeHead(301,{Location: 'index.html'});
+  res.end();
 });
   
-  
-    function Getir(ip) {
-    //var sum = parseInt(a) + parseInt(b);
-    alert(ip);
-  }
-  ;
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
