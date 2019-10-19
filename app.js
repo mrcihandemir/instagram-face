3b72cf7b9093369563d74d747e34ad67996a6731
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-20 00:56:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  //res.statusCode = 200;
-  //res.setHeader('Content-Type', 'text/html');
-  //res.end('<h1>profile name: <input type="text" name="ip" id="ip"><br><button onclick="Getir(document.getElementById("ip").value)">Getir</button></h1>');
-  //res.writeHead(301,{Location: 'index.html'});
-  res.redirect('index.html');
-  res.end();
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  var sayfa = '<html><head><title>instagram face</title>';
+  sayfa = sayfa + "<script>function Getir(ip) { alert(ip);}</script>";
+  sayfa = sayfa + '</head>';
+  sayfa = sayfa + '<body>';
+  sayfa = sayfa + '<h1>profile name: <input type="text" name="ip" id="ip"><br><button onclick="Getir(document.getElementById("ip").value)">Getir</button></h1>';
+  sayfa = sayfa + '</body></html>';
+  res.end(sayfa);
 });
   
 
