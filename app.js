840d6d268159470db32cf6c7bdf13b013e4e4ae5
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-20 00:33:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>profile name: <input type="text" name="ip" id="ip"><br>
-          <button onclick="Getir(document.getElementById("ip").value)">Getir</button>
-          </h1>');
+  res.end('<h1>profile name: <input type="text" name="ip" id="ip"><br><button onclick="Getir(document.getElementById("ip").value)">Getir</button></h1>');
 });
   
   
