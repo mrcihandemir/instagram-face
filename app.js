a0882b5ae68951ccbf1449d270b7a1df093e03ec
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-20 18:27:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const server = http.createServer((req, res) => {
   sayfa = sayfa + '</body></html>';
   res.end(sayfa);
   */
-	fs.readFile("index.html", function(err, data){
+	fs.readFile("./index.html", function(err, data){
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write(data);
   response.end();
