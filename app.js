8e716ff5d84a9585474855ab6102a96f7669e69f
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-20 22:06:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express(); 
 const port = process.env.PORT || 80
 
-//app.use(express.static('app'));
+app.use(express.static('app'));
 
 app.get('/', function (req, res) {
   // res.send('Hello World');
-  //res.sendfile('index.html');
-  res.send('hell');
+  res.sendfile('index.html');
+  //res.send('hell');
 });
 
 app.listen(port, function(){
