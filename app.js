a918e65679511ba109d94157383e29ac42189405
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-20 18:22:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || 3000
-var http = require('http');  
+
+/*var http = require('http');  
 var url = require('url');  
 var fs = require('fs'); 
-
+*/
 
 /*
 http.createServer(function(request, response) {
