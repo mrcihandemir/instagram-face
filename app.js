97fe63a34f15c6eb582de2b9fd53ed674d7916b6
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-20 21:19:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('app'));
 
 app.get('/', function (req, res) {
   // res.send('Hello World');
