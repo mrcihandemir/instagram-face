ff4c55f780a2f27b9c52259e149dac142b683e10
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-22 16:02:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,40 @@ app.get('/', function (req, res) {
 app.listen(port, function(){
   console.log("Listening port!")
 });
+
+
+app.post('/fetch_external_image', async (req, res) => {
+  const { imageUrl } = req.body
+  if (!imageUrl) {
+    return res.status(400).send('imageUrl param required')
+  }
+  try {
+    const externalResponse = await request(imageUrl)
+    res.set('content-type', externalResponse.headers['content-type'])
+    return res.status(202).send(Buffer.from(externalResponse.body))
+  } catch (err) {
+    return res.status(404).send(err.toString())
+  }
+})
+
+function request(url, returnBuffer = true, timeout = 10000) {
+  return new Promise(function(resolve, reject) {
+    const options = Object.assign(
+      {},
+      {
+        url,
+        isBuffer: true,
+        timeout,
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+      },
+      returnBuffer ? { encoding: null } : {}
+    )
+
+    get(options, function(err, res) {
+      if (err) return reject(err)
+      return resolve(res)
+    })
+  })
+}
