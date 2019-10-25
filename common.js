9238f33616d3e0c7381494cedac44e719d40a15c
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-10-25 15:26:57 +0300

    Update common.js

--- common.js
+++ common.js
@@ -27,36 +27,3 @@ async function requestExternalImage(imageUrl) {
 */
 
 
-
-async function requestInstagramProfilePic(accountName) {
-  console.log("POSTMAN2");
-  console.log(JSON.stringify({ accountName }));
-  const res = await fetch('instagram', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ accountName })
-  })
-  //console.log(body);
-  if (!(res.status < 400)) {
-    console.error(res.status + ' : ' + await res.text())
-    throw new Error('failed to fetch instagram profile pic from url: ' + accountName)
-  } else {
-    console.log(res)
-  }    
-  
-  let blob
-  try {
-    blob = await res.blob()
-    const img =  await faceapi.bufferToImage(blob)
-    //$('#gandhi').get(0).src = img.src ;
-    //await updateResults();
-    return await faceapi.bufferToImage(blob)
-  } catch (e) {
-    console.error('received blob:', blob)
-    console.error('error:', e)
-    throw new Error('failed to load image from url: ' + imageUrl)
-  }
-
-}
